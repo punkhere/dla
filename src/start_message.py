9797bd5066e5cc87b720861f575b03dc025c74f9
punkhere <79733447+punkhere@users.noreply.github.com>
2021-07-12 15:10:31 +0530

    Update start_message.py

--- src/start_message.py
+++ src/start_message.py
@@ -13,7 +13,7 @@ def start_message(client, message):
         [InlineKeyboardButton("Instructions", callback_data="instructions")],
     ]
     reply_markup = InlineKeyboardMarkup(kkeeyyb)
-    pic_url = "https://drive.google.com/file/d/1TzmuSj05qF6Sm5Z_CSm3AGUCBx4Oe0F0/view?usp=sharing"
+    pic_url = "https://telegra.ph/file/225dc27e5232e7c4160f2.jpg"
     message.reply_photo(pic_url, caption=f"""**Hi {message.chat.first_name}**,
 
 Welcome to Mizuhara Bot, Here you can Download all Anime for FREE 😁 ,For TG Anime Join @watchlistonfire , @watchlist_on_fire
